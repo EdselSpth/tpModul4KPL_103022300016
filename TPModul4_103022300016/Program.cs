08efba49589b3a72b0f424a34dc6ee7649fe4931
edselspth <edselspth@gmail.com>
2025-03-09 13:27:20 +0700

    menambahkan implementasi dengan table-drive

--- TPModul4_103022300016/Program.cs
+++ TPModul4_103022300016/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TPModul4_103022300016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Daftar Kelurahan");
+            Console.WriteLine("===================================");
+            Console.WriteLine("Kelurahan \t |    Kode Pos");
+            Console.WriteLine("===================================");
+            foreach (KodePos.kelurahan kel in Enum.GetValues(typeof(KodePos.kelurahan)))
+            {
+                String namaKelurahan = kel.ToString();
+                Console.WriteLine($"{kel,-16} |     {KodePos.getkodePos(kel)}");
+            }
+        }
+    }
+}
